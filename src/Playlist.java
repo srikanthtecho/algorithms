9c85db6240d0cb442a20b4fff3be1fda16398a80
srikanth malyaala <srikanth malyaala>
2017-04-11 19:57:42 +0530

    Commit playlist algo

--- src/Playlist.java
+++ src/Playlist.java
@@ -1,7 +1,56 @@
-package PACKAGE_NAME;
-
 /**
  * Created by Lenovo on 11-04-2017.
  */
 public class Playlist {
+
+    public static void main(String args[]) {
+
+        String[] songs = new String[]{"wheniseeyouagin", "borntorun", "nothingelsematters", "cecelia"};
+
+        int buttonClicks = playlist(songs, 1, "cecelia");
+
+        if (buttonClicks == 2) {
+            System.out.println("TEST PASS");
+        } else {
+            System.out.println("TEST FAIL - Expected output 2 but " + buttonClicks);
+        }
+    }
+
+    private static int playlist(final String songs[], final int k, final String q) {
+
+        int clockwiseCount = 0;
+        int index = k;
+
+        int total = songs.length;
+        for (; true; index++) {
+
+            if (index == total) {
+                index = 0;
+            }
+
+            if (songs[index].equals(q)) {
+                break;
+            }
+            clockwiseCount = clockwiseCount + 1;
+        }
+
+        int antiClockwiseCount = 0;
+        index = k;
+        for (; true; index--) {
+
+            if (songs[index].equals(q)) {
+                break;
+            }
+
+            if (index == 0) {
+                index = total;
+            }
+
+            antiClockwiseCount = antiClockwiseCount + 1;
+        }
+
+        return Math.min(clockwiseCount, antiClockwiseCount);
+    }
+
+
 }
