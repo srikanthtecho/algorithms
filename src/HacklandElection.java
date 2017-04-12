3ca7189c7c9c355d7823f33ded160f46ba70469d
srikanth malyaala <srikanth malyaala>
2017-04-12 11:23:38 +0530

    Add hackland collection

---
+++ src/HacklandElection.java
@@ -0,0 +1,45 @@
+import java.util.*;
+
+/**
+ * Created by Lenovo on 06-04-2017.
+ */
+public class HacklandElection {
+
+    public static void main(String args[]) {
+
+        final String[] candidates = new String[]{"Alex", "Michael",
+                "Harry", "Dave", "Michael", "Victor", "Harry", "Alex", "Mary", "Mary"};
+
+        final String electionWinner = electionWinner(candidates);
+        System.out.println(electionWinner);
+    }
+
+    private static String electionWinner(String[] candidates) {
+
+        final Map<String, Integer> candidateVotes = new HashMap<>();
+        int maxVotes = 0;
+
+        for (final String candidate : candidates) {
+            if (candidateVotes.get(candidate) == null) {
+                candidateVotes.put(candidate, 1);
+            } else {
+                final int votes = candidateVotes.get(candidate) + 1;
+                if (votes > maxVotes) {
+                    maxVotes = votes;
+                }
+                candidateVotes.put(candidate, votes);
+            }
+        }
+
+        final SortedSet<String> sortedCandidates = new TreeSet<>();
+
+        for (final Map.Entry<String, Integer> entry : candidateVotes.entrySet()) {
+
+            if (entry.getValue() == maxVotes) {
+                sortedCandidates.add(entry.getKey());
+            }
+        }
+
+        return sortedCandidates.last();
+    }
+}
